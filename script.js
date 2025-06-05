5818d13ab90271c132e8fd3da4c94aef91991ec2
Cheyenne <cheyenne.mt.arnold@gmail.com>
2025-06-05 07:41:00 -0700

    learning js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // Example: Add functionality for a search bar or interactivity as needed
 console.log("Kit Lounge script loaded!");
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
